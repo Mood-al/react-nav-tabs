b81c777e2504d017538fd9ed238adedf7581532e
Mohammad Aliwi <aliwi299@gmail.com>
2022-04-09 08:42:05 +0300

    finalizing the v1

--- components/Tabs/CutomTabs.js
+++ components/Tabs/CutomTabs.js
@@ -1,26 +1,33 @@
 import styled from "@emotion/styled";
-import { useEventCallback } from "@mui/material";
-import {
-  detectScrollType,
-  getNormalizedScrollLeft,
-} from "normalize-scroll-left";
-import React, { useCallback, useContext, useLayoutEffect } from "react";
+import { useEventCallback } from "../../hooks/useEventCallback";
+import { getNormalizedScrollLeft } from "../../utils/getNormalizedScrollLeft";
+import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import { useRef } from "react";
-import { RTLContext } from "../../context/RTLContext";
+
 import animate from "../../utils/animate";
 import { debounce } from "../../utils/debounce";
+import RightArrow from "../Arrows/RightArrow";
+import LeftArrow from "../Arrows/LeftArrow";
 
-const getScrollPosition = (el = window) => ({
-  x: el.pageXOffset !== undefined ? el.pageXOffset : el.scrollLeft,
-  //   y: el.pageYOffset !== undefined ? el.pageYOffset : el.scrollTop,
-});
-
-let warnedOnceTabPresent = false;
 const defaultIndicatorStyle = {};
 
-const CutomTabs = ({ onTabClick, activeTab, isRTL, children }) => {
+const CutomTabs = ({
+  onTabClick,
+  activeTab,
+  isRTL,
+  children,
+  action,
+  tabsScrollAmount = 3,
+  didReachEnd = () => null,
+  didReachStart = () => null,
+  animationDuration = 300,
+  navBtnCLickAnimationDuration = 300,
+  selectedAnimationDuration = 300,
+  rightBtnIcon = <RightArrow />,
+  leftBtnIcon = <LeftArrow />,
+}) => {
   const tabsRef = useRef();
   const tabRef = useRef([]);
 
@@ -29,8 +36,6 @@ const CutomTabs = ({ onTabClick, activeTab, isRTL, children }) => {
     end: false,
   });
   const [indicatorStyle, setIndicatorStyle] = useState(defaultIndicatorStyle);
-
-  const valueToIndex = new Map();
 
   const getTabsRects = () => {
     const tabsNode = tabsRef.current;
@@ -63,43 +68,7 @@ const CutomTabs = ({ onTabClick, activeTab, isRTL, children }) => {
       if (children.length > 0) {
         const tab = tabRef.current[activeTab];
 
-        if (process.env.NODE_ENV !== "production") {
-          if (!tab) {
-            console.error(
-              [
-                `rn-tabs: The \`activeTab\` provided to the Tabs component is invalid.`,
-                `None of the Tabs' children match with "${activeTab}".`,
-                valueToIndex.keys
-                  ? `You can provide one of the following values: ${Array.from(
-                      valueToIndex.keys()
-                    ).join(", ")}.`
-                  : null,
-              ].join("\n")
-            );
-          }
-        }
-
         tabRect = tab ? tab.getBoundingClientRect() : null;
-
-        if (process.env.NODE_ENV !== "production") {
-          if (
-            process.env.NODE_ENV !== "test" &&
-            !warnedOnceTabPresent &&
-            tabRect &&
-            tabRect.width === 0 &&
-            tabRect.height === 0
-          ) {
-            tabsRects = null;
-            console.error(
-              [
-                "rn-tabs: The `value` provided to the Tabs component is invalid.",
-                `The Tab with this \`value\` ("${activeTab}") is not part of the document layout.`,
-                "Make sure the tab item is present in the document or that it's not `display: none`.",
-              ].join("\n")
-            );
-            warnedOnceTabPresent = true;
-          }
-        }
       }
     }
 
@@ -162,10 +131,7 @@ const CutomTabs = ({ onTabClick, activeTab, isRTL, children }) => {
       tabsRef.current,
       isRTL ? "rtl" : "ltr"
     ); // use 1 for the potential rounding error with browser zooms.
-    console.log(Math.floor(scrollLeft.toFixed(2)), "ddddddddddddddddddddddd");
-    console.log(
-      Math.ceil(scrollLeft.toFixed(2)) < scrollWidth - clientWidth - 1
-    );
+
     showStartScroll = Math.floor(scrollLeft.toFixed(2)) > 1;
     showEndScroll =
       Math.ceil(scrollLeft.toFixed(2)) < scrollWidth - clientWidth - 1;
@@ -178,20 +144,18 @@ const CutomTabs = ({ onTabClick, activeTab, isRTL, children }) => {
         start: showStartScroll,
         end: showEndScroll,
       });
+      didReachEnd(!showEndScroll);
+      didReachStart(!showStartScroll);
     }
   });
 
-  const posRef = useRef(0);
   const onRightBtnClick = () => {
-    // if (tabsRef.current.clientWidth < tabsRef.current.scrollWidth) {
-    //   scroll(+tabRef.current[0].offsetWidth);
-    //   // tabsRef.current.scrollLeft += tabRef.current[0].offsetWidth;
-    // }
-
-    // moveTabsScroll(tabsRef.current.clientWidth);
     scroll(
-      tabsRef.current.scrollLeft + tabRef.current[activeTab].clientWidth * 2,
-      300
+      tabsRef.current.scrollLeft +
+        tabRef.current[activeTab].clientWidth * tabsScrollAmount,
+      navBtnCLickAnimationDuration
+        ? navBtnCLickAnimationDuration
+        : animationDuration
     );
   };
 
@@ -203,10 +167,22 @@ const CutomTabs = ({ onTabClick, activeTab, isRTL, children }) => {
     // }
     // moveTabsScroll(-1 * tabsRef.current.clientWidth);
     scroll(
-      tabsRef.current.scrollLeft - tabRef.current[activeTab].clientWidth * 2,
-      300
+      tabsRef.current.scrollLeft -
+        tabRef.current[activeTab].clientWidth * tabsScrollAmount,
+      navBtnCLickAnimationDuration
+        ? navBtnCLickAnimationDuration
+        : animationDuration
     );
   };
+
+  React.useImperativeHandle(
+    action,
+    () => ({
+      onLeftBtnClick,
+      onRightBtnClick,
+    }),
+    [onLeftBtnClick, onRightBtnClick]
+  );
 
   const onNativeTabClick = (e, index) => {
     onTabClick(e, index);
@@ -216,7 +192,7 @@ const CutomTabs = ({ onTabClick, activeTab, isRTL, children }) => {
     console.log(scrollValue);
     if (animation) {
       animate("scrollLeft", tabsRef.current, scrollValue, {
-        duration: duration ? duration : 500,
+        duration: duration ? duration : 300,
       });
     } else {
       tabsRef.current.scrollLeft = scrollValue;
@@ -238,7 +214,12 @@ const CutomTabs = ({ onTabClick, activeTab, isRTL, children }) => {
         (tabRef.current[activeTab].getBoundingClientRect()[start] -
           tabsRef.current.getBoundingClientRect()[start]);
       // tabsRef.current.scrollLeft = nextScrollStart;
-      scroll(nextScrollStart, 300);
+      scroll(
+        nextScrollStart,
+        selectedAnimationDuration
+          ? selectedAnimationDuration
+          : animationDuration
+      );
     } else if (
       tabRef.current[activeTab].getBoundingClientRect()[end] >
       tabsRef.current.getBoundingClientRect()[end]
@@ -249,7 +230,12 @@ const CutomTabs = ({ onTabClick, activeTab, isRTL, children }) => {
         (tabRef.current[activeTab].getBoundingClientRect()[end] -
           tabsRef.current.getBoundingClientRect()[end]);
       // tabsRef.current.scrollLeft = nextScrollStart;
-      scroll(nextScrollStart, 300);
+      scroll(
+        nextScrollStart,
+        selectedAnimationDuration
+          ? selectedAnimationDuration
+          : animationDuration
+      );
     }
   });
 
@@ -283,29 +269,60 @@ const CutomTabs = ({ onTabClick, activeTab, isRTL, children }) => {
 
   return (
     <StyledTabsContainer>
-      {displayScroll.end && (
-        <button className="right" onClick={onRightBtnClick}>
-          <span dir="ltr"> {">"}</span>{" "}
+      {isRTL ? (
+        <button
+          disabled={!displayScroll.end}
+          className="right"
+          onClick={onRightBtnClick}
+        >
+          {rightBtnIcon}
+        </button>
+      ) : (
+        <button
+          disabled={!displayScroll.start}
+          className="left"
+          onClick={onLeftBtnClick}
+        >
+          {leftBtnIcon}
         </button>
       )}
 
-      <StyledCutomTabs ref={tabsRef} onScroll={handleTabsScroll}>
-        {[...Array(40).keys()].map((item, index) => (
-          <div
-            key={item}
-            onClick={(e) => onNativeTabClick(e, index)}
-            ref={(el) => (tabRef.current[index] = el)}
-            className={`tab  ${activeTab === index ? "bg-primary" : ""}`}
-          >
-            item {item}
-          </div>
-        ))}
+      <StyledCutomTabs
+        ref={tabsRef}
+        role="tablist"
+        aria-label="tabs"
+        onScroll={handleTabsScroll}
+      >
+        <>
+          {React.Children.map(children, (child, index) =>
+            React.cloneElement(child, {
+              ref: (ref) => (tabRef.current[index] = ref),
+              onClick: (e) => onNativeTabClick(e, index),
+              role: "tab",
+              ["aria-selected"]: activeTab === index ? "true" : "false",
+              ["aria-controls"]: `panel-${index}`,
+              id: `tab-${index}`,
+              tabIndex: activeTab === index ? "0" : "-1",
+            })
+          )}
+        </>
       </StyledCutomTabs>
 
-      {displayScroll.start && (
-        <button className="left" onClick={onLeftBtnClick}>
-          {" "}
-          <span dir="ltr"> {"<"}</span>{" "}
+      {isRTL ? (
+        <button
+          disabled={!displayScroll.start}
+          className="left"
+          onClick={onLeftBtnClick}
+        >
+          {leftBtnIcon}
+        </button>
+      ) : (
+        <button
+          disabled={!displayScroll.end}
+          className="right"
+          onClick={onRightBtnClick}
+        >
+          {rightBtnIcon}
         </button>
       )}
     </StyledTabsContainer>
@@ -318,8 +335,8 @@ const StyledCutomTabs = styled.div`
   /* scroll-behavior: smooth; */
   background: red;
   display: flex;
-  overflow: auto;
-  position: relative;
+  overflow: hidden;
+  /* position: relative; */
   .tab {
     padding: 10px 40px;
     white-space: nowrap;
@@ -330,8 +347,9 @@ const StyledCutomTabs = styled.div`
 const StyledTabsContainer = styled.div`
   padding: 0 25px;
   position: relative;
+  display: flex;
   button {
-    position: absolute;
+    /* position: absolute;
     top: 50%;
     transform: translate(0, -50%);
     &.right {
@@ -339,6 +357,6 @@ const StyledTabsContainer = styled.div`
     }
     &.left {
       left: -0px;
-    }
+    } */
   }
 `;
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,26 @@
 import { Container } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { useRouter } from "next/dist/client/router";
-import { useContext, useState } from "react";
-import MatTabs from "../components/MatTabs";
+import { useContext, useRef, useState } from "react";
+
 import CustomTabs from "../components/Tabs/CutomTabs";
 import { RTLContext } from "../context/RTLContext";
 
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const router = useRouter();
-
-  const theme = createTheme({
-    direction: "rtl", // Both here and <body dir="rtl">
-  });
-
-  const [activeTab, setActiveTab] = useState(8);
+  const [activeTab, setActiveTab] = useState(2);
   const onTabClick = (e, index) => {
     setActiveTab(index);
   };
   const { isRTL } = useContext(RTLContext);
+  const ref = useRef();
+  console.log(ref);
+  const [isLeftArrowDisapled, setIsLeftArrowDisabled] = useState(false);
+  const [isRightArrowDisapled, setIsRightArrowDisabled] = useState(false);
 
+  const didReachEnd = (val) => setIsRightArrowDisabled(val);
+  const didReachStart = (val) => setIsLeftArrowDisabled(val);
   return (
     <div className="container">
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae nemo
@@ -28,7 +28,60 @@ export default function Home() {
       aliquid molestias saepe officia aspernatur adipisci? Fuga cumque esse
       deleniti nemo molestias.
       {/* <MatTabs /> */}
-      <CustomTabs activeTab={activeTab} onTabClick={onTabClick} isRTL={isRTL} />
+      <CustomTabs
+        // the props returns a group of events to control the tabs such as onLeftBtnClick
+        action={ref}
+        activeTab={activeTab}
+        onTabClick={onTabClick}
+        isRTL={isRTL}
+        // set if the tabs reached the end point of the tab container
+        didReachEnd={didReachEnd}
+        // sets if the tabs reached the start point container
+        didReachStart={didReachStart}
+        // sets how many tabs you want to scroll on every move
+        // default 3 tabs on each navigation button click
+        tabsScrollAmount={3}
+        // set the general animation duration when you click on the navigation buttons and when you click out the tabs view
+        // default 300s
+        animationDuration={300}
+        // sets the animation of the scroll when you click on the navigation buttons
+        // note : this will overwirte the animationDuration value
+        // default 300s
+        navBtnCLickAnimationDuration={300}
+        // sets the animation of the scroll when you click on a tab that is out of the view
+        // note : this will overwirte the animationDuration value
+        // default 300s
+        selectedAnimationDuration={300}
+        // sets the right navitgation vutton icon
+        // default feather arrow-right svg icon
+        rightBtnIcon={">"}
+        // sets the left navitgation vutton icon
+        // default feather arrow-left svg icon
+        leftBtnIcon={"<"}
+      >
+        {[...Array(20).keys()].map((item, index) => (
+          <div
+            key={item}
+            onClick={(e) => onNativeTabClick(e, index)}
+            ref={(el) => (tabRef.current[index] = el)}
+            className={`tab  ${activeTab === index ? "bg-primary" : ""}`}
+          >
+            item {item}
+          </div>
+        ))}
+      </CustomTabs>
+      <button
+        disabled={isLeftArrowDisapled}
+        onClick={() => ref.current.onLeftBtnClick()}
+      >
+        left
+      </button>
+      <button
+        disabled={isRightArrowDisapled}
+        onClick={() => ref.current.onRightBtnClick()}
+      >
+        right
+      </button>
     </div>
   );
 }
