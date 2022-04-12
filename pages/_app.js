92e598b3f633512fcf27c028e84e8a28b7cfba25
Mohammad Aliwi <aliwi299@gmail.com>
2022-04-12 22:32:47 +0300

    testing the changes out

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
 import "../styles/globals.css";
-import { useEffect, useState } from "react";
-import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
-import { create } from "jss";
-import rtl from "jss-rtl";
-import { StylesProvider, jssPreset } from "@mui/styles";
+
 import { RTLProvider } from "../context/RTLContext";
 import Head from "next/head";
 import "../styles/rn-tabs.css";
+import "../styles/main.css";
+
 import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
@@ -17,7 +15,7 @@ function MyApp({ Component, pageProps }) {
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         />
       </Head>
       <RTLProvider>
