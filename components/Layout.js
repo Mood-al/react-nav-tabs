de44b2a256c2f80af0d0f1d282c8111812f04e09
Mooder <63562308+Mood-al@users.noreply.github.com>
2023-01-21 23:55:22 +0100

    add some changes

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import Footer from "./Footer";
+import FooterBanner from "./FooterBanner";
 import Navbar from "./Navbar";
 
 const Layout = ({ children }) => {
@@ -6,6 +7,35 @@ const Layout = ({ children }) => {
     <div>
       <Navbar />
       {children}
+      <div className="border mb-3" />
+      <div className="container mb-3">
+        <div className="border-top border-bottom border-2 border-danger fs-5 p-3 text-wrap">
+          Please consider giving me a star on the{" "}
+          <a
+            className="text-warning"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/Mood-al/react-tabs-scrollable"
+          >
+            Github
+          </a>
+          's repo it will give me motiviation to do more packages like this!
+        </div>
+      </div>
+      <FooterBanner>
+        <p className="lead display-5 text-center text-white">
+          Have you checked my new package{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-primary"
+            href="https://github.com/Mood-al/svelte-tabs-scrollable"
+          >
+            svelte-tabs-scrollable
+          </a>
+          ?
+        </p>
+      </FooterBanner>
       <Footer />
     </div>
   );
