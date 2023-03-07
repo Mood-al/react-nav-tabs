f3e7b88a12a81bcde1e72d53bec38a3ebb2d6121
Mooder <63562308+Mood-al@users.noreply.github.com>
2023-03-07 01:24:36 +0100

    adding new changes to the ui

--- pages/_app.js
+++ pages/_app.js
@@ -6,9 +6,9 @@ import Layout from "../components/Layout";
 import { NextSeo } from "next-seo";
 import DefaultLayout from "../layouts/DefaultLayout";
 import "react-tabs-scrollable/dist/rts.css";
-import "../styles/bootstrap.css";
-import "nprogress/nprogress.css";
 import "../styles/main.css";
+import "nprogress/nprogress.css";
+import "../styles/bootstrap.css";
 
 NProgress.configure({
   showSpinner: false,
